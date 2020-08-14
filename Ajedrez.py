24b1c5f503072a0f5a785f328457ff4a11e32365
luisfesalgado23 <58190338+luisfesalgado23@users.noreply.github.com>
2020-08-14 17:49:41 -0500

    Update Ajedrez.py
    Funciones para llenar los espaciones diagonales para el alfil y los verticales y horizontales para la torre, ambos funcionan para la reina

--- Ajedrez.py
+++ Ajedrez.py
@@ -14,12 +14,68 @@ class Pieza:
         self.fila=fila                          ## fila en la que se encuentra ubicada
         self.columna=columna                    ## columna donde se encuentra ubicada
         self.tipo=tipo                          ## tipo de pieza entre las existentes en el ajedrez
+        self.tablero=np.zeros((8,8))            ## creación del tablero
         
-    def movimiento(self,fila,columna):         ## función que realiza un cambio de posición en el tablero
-        self.fila=fila
-        self.columna=columna
+    def movimiento(self,fila,columna):          ## función que realiza un cambio de posición en el tablero
+        if self.tablero[fila][columna]==0:
+            print('El movimiento no es valido')
+            
+        else:
+            self.fila=fila
+            self.columna=columna
+            for i in range(8):                      ## restaurar los valores del tablero
+                for j in range(8):
+                    self.tablero[i][j]=0
+    
+    def diagonal(self):                                 ## funcion que llena los movimientos en diagnal
+
+        i=8-self.fila
+        j=self.columna-1
 
 
+        self.tablero[i][j]=8
+    
+        while j<7 and i>0:             ## llenando diagonal superior derecha
+            self.tablero[i-1][j+1]=1
+            i=i-1
+            j=j+1
+        
+        i=8-self.fila
+        j=self.columna-1
+    
+        while j>0 and i>0:             ## llenando diagonal superior izquierda
+            self.tablero[i-1][j-1]=1
+            i=i-1
+            j=j-1
+        
+        i=8-self.fila
+        j=self.columna-1
+    
+        while j>0 and i<7:             ## llenando diagonal inferior izquierda
+            self.tablero[i+1][j-1]=1
+            i=i+1
+            j=j-1
+        
+        i=8-self.fila
+        j=self.columna-1
+    
+        while j<7 and i<7:             ## llenando diagonal inferior derecha
+            self.tablero[i+1][j+1]=1
+            i=i+1
+            j=j+1
+
+    def ver_hor(self):
+        i=8-self.fila
+        j=self.columna-1
+    
+        for k in range(8):             ## llenando columna
+            self.tablero[i][k]=1
+      
+        for k in range(8):             ## llenando fila
+            self.tablero[k][j]=1
+           
+        self.tablero[i][j]=8        
+        
 ## obtener datos
 
 print('El tablero de ajedrez está compuesto por filas y columnas, las filas están enumeradas del 1-8 de manera descendente. Las colunas por otro lado son nombradas con letras de la a-h, a continuación, se muestra un ejemplo: \n')
@@ -55,5 +111,18 @@ while t!=1 and t!=2 and t!=3 and t!=4 and t!=5 and t!=6:
    print('La opción no es correcta intente nuevamente') 
    t=int(input())
 
+#%%
 
+pieza = Pieza(f, ord(c)-96, t)
 
+if pieza.tipo==4:
+    pieza.diagonal()
+    
+if pieza.tipo==3:
+    pieza.ver_hor()
+
+if pieza.tipo==2:
+    pieza.diagonal()
+    pieza.ver_hor()
+    
+print (pieza.tablero)
