eefa02d661b49c62fe7b94c261985eec34f6ac6e
luisfesalgado23 <58190338+luisfesalgado23@users.noreply.github.com>
2020-08-14 22:48:15 -0500

    Update Ajedrez.py
    Creacion de las funciones para los movimientos del rey y el caballo

--- Ajedrez.py
+++ Ajedrez.py
@@ -4,26 +4,24 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
-import pandas as pd
 import numpy as np
 
 ## creación de clase 
 class Pieza:
-    def __init__(self,fila,columna,tipo):       ## atributos de las piezas del juego
-        self.fila=fila                          ## fila en la que se encuentra ubicada
-        self.columna=columna                    ## columna donde se encuentra ubicada
-        self.tipo=tipo                          ## tipo de pieza entre las existentes en el ajedrez
-        self.tablero=np.zeros((8,8))            ## creación del tablero
+    def __init__(self,fila,columna,tipo):          ## atributos de las piezas del juego
+        self.fila=fila                                  ## fila en la que se encuentra ubicada
+        self.columna=columna                            ## columna donde se encuentra ubicada
+        self.tipo=tipo                                  ## tipo de pieza entre las existentes en el ajedrez
+        self.tablero=np.zeros((8,8))                    ## creación del tablero
         
-    def movimiento(self,fila,columna):          ## función que realiza un cambio de posición en el tablero
-        if self.tablero[fila][columna]==0:
+    def movimiento(self,fila,columna):                  ## función que realiza un cambio de posición en el tablero
+        if self.tablero[fila][columna]==0:      
             print('El movimiento no es valido')
             
         else:
             self.fila=fila
             self.columna=columna
-            for i in range(8):                      ## restaurar los valores del tablero
+            for i in range(8):                          ## restaurar los valores del tablero
                 for j in range(8):
                     self.tablero[i][j]=0
     
@@ -35,7 +33,7 @@ class Pieza:
 
         self.tablero[i][j]=8
     
-        while j<7 and i>0:             ## llenando diagonal superior derecha
+        while j<7 and i>0:                              ## llenando diagonal superior derecha
             self.tablero[i-1][j+1]=1
             i=i-1
             j=j+1
@@ -43,7 +41,7 @@ class Pieza:
         i=8-self.fila
         j=self.columna-1
     
-        while j>0 and i>0:             ## llenando diagonal superior izquierda
+        while j>0 and i>0:                             ## llenando diagonal superior izquierda
             self.tablero[i-1][j-1]=1
             i=i-1
             j=j-1
@@ -51,7 +49,7 @@ class Pieza:
         i=8-self.fila
         j=self.columna-1
     
-        while j>0 and i<7:             ## llenando diagonal inferior izquierda
+        while j>0 and i<7:                             ## llenando diagonal inferior izquierda
             self.tablero[i+1][j-1]=1
             i=i+1
             j=j-1
@@ -59,7 +57,7 @@ class Pieza:
         i=8-self.fila
         j=self.columna-1
     
-        while j<7 and i<7:             ## llenando diagonal inferior derecha
+        while j<7 and i<7:                             ## llenando diagonal inferior derecha
             self.tablero[i+1][j+1]=1
             i=i+1
             j=j+1
@@ -68,14 +66,258 @@ class Pieza:
         i=8-self.fila
         j=self.columna-1
     
-        for k in range(8):             ## llenando columna
+        for k in range(8):                            ## llenando columna
             self.tablero[i][k]=1
       
-        for k in range(8):             ## llenando fila
+        for k in range(8):                            ## llenando fila
             self.tablero[k][j]=1
            
         self.tablero[i][j]=8        
+
+    def rey(self):
+        i=8-self.fila
+        j=self.columna-1
+    
+        if i!=0 and i!=7 and j!=0 and j!=7:         ##cualquier posicion fuera de los extremos
+            for k in range(3):
+                for n in range(3):
+                    self.tablero[i+k-1][j+n-1]=1
+            self.tablero[i][j]=8
         
+        if i==0 and j==0:                           ## esquina inferior izquierda
+            for k in range(2):
+                for n in range(2):
+                    self.tablero[i+k][j+n]=1
+            self.tablero[i][j]=8
+        
+        if i==7 and j==0:                           ## esquina superior izquierda
+            for k in range(2):
+                for n in range(2):
+                    self.tablero[i+k-1][j+n]=1
+            self.tablero[i][j]=8
+        
+        if i==0 and j==7:                           ## esquina inferior derecha del tablero
+            for k in range(2):
+                for n in range(2):
+                    self.tablero[i+k][j+n-1]=1
+            self.tablero[i][j]=8
+        
+        if i==7 and j==7:                           ## esquina superior derecha del tablero
+            for k in range(2):
+                for n in range(2):
+                    self.tablero[i+k-1][j+n-1]=1
+            self.tablero[i][j]=8
+        
+        if i==0 and j!=0 and j!=7:                  ## extremo inferior del tablero             
+            for k in range(2):
+                for n in range(3):
+                    self.tablero[i+k][j+n-1]=1
+            self.tablero[i][j]=8
+        
+        if i==7 and j!=0 and j!=7:                  ## extremo superior del tablero
+            for k in range(2):
+                for n in range(3):
+                    self.tablero[i+k-1][j+n-1]=1
+            self.tablero[i][j]=8
+        
+        if j==0 and i!=0 and i!=7:                  ## extremo izquierdo del tablero
+            for k in range(3):
+                for n in range(2):
+                    self.tablero[i+k-1][j+n]=1
+            self.tablero[i][j]=8
+        
+        if j==7 and i!=0 and i!=7:                  ## extremo derecho del tablero
+            for k in range(3):
+                for n in range(2):
+                    self.tablero[i+k-1][j+n-1]=1
+            self.tablero[i][j]=8
+    
+    def caballo(self):
+        i=8-self.fila
+        j=self.columna-1
+        bandera =0
+    
+    ## region 1 para movimientos del caballo
+    
+        if j>1 and j<6 and i>1 and i<6:
+            for k in range(0,5,4):
+                for n in range(0,3,2):
+                    self.tablero[i+k-2][j+n-1]=1
+            for k in range(0,3,2):
+                for n in range(0,5,4):
+                    self.tablero[i+k-1][j+n-2]=1
+            self.tablero[i][j]=8
+
+    ## region 2 para movimientos del caballo
+
+        if i>1 and i<6:
+        
+            if j==1:
+                m=2
+                bandera=1
+            if j==6:
+                m=-2
+                bandera=2
+            if bandera==1:
+                for k in range(0,5,4):
+                    for n in range(0,3,2):
+                        self.tablero[i+k-2][j+n-1]=1
+                for n in range(0,3,2):
+                    self.tablero[i+n-1][j+m]=1
+                self.tablero[i][j]=8
+                bandera=0
+            
+            if j>1 and j<6:
+        
+                if i==1:
+                    m=2
+                    bandera=1
+                if i==6:
+                    m=-2
+                    bandera=1
+                if bandera==1:
+                    for n in range(0,3,2):
+                        self.tablero[i+m][j+n-1]=1
+                    for k in range(0,3,2):
+                        for n in range(0,5,4):
+                            self.tablero[i+k-1][j+n-2]=1
+                    self.tablero[i][j]=8
+                    bandera=0
+     
+            
+         ## region 4 para movimientos del caballo
+        if i>1 and i<6:
+        
+             if j==0:
+                 m=2
+                 o=1
+                 bandera=1
+             if j==7:
+                m=-2
+                o=-1
+                bandera=1
+             if bandera==1:
+                 for k in range(0,5,4):
+                     self.tablero[i+k-2][j+o]=1
+                 for n in range(0,3,2):
+                     self.tablero[i+n-1][j+m]=1
+                 self.tablero[i][j]=8
+                 bandera=0
+
+        if j>1 and j<6:
+        
+             if i==0:
+                 m=2
+                 o=1
+                 bandera=1
+             if i==7:
+                 m=-2
+                 o=-1
+                 bandera=1
+             if bandera==1:
+                for n in range(0,5,4):
+                    self.tablero[i+o][j+n-2]=1
+                for n in range(0,3,2):
+                    pieza.tablero[i+m][j+n-1]=1
+                self.tablero[i][j]=8
+                bandera=0
+    
+    ## region 3 para movimientos del caballo
+    
+        if i==j:
+            if j==1:
+               o=2
+               bandera=1
+            if j==6:
+                o=-2
+                bandera=1
+            
+            if bandera==1:
+                for n in range(0,3,2):
+                    self.tablero[i+o][j+n-1]=1
+                for n in range(0,3,2):
+                    self.tablero[i+n-1][j+o]=1    
+                self.tablero[i][j]=8  
+                bandera=0
+        
+            
+        if i==1 and  j==6:
+            for n in range(0,3,2):
+                self.tablero[i+2][j+n-1]=1
+            for n in range(0,3,2):
+                self.tablero[i+n-1][j-2]=1    
+            self.tablero[i][j]=8  
+            
+        if i==6 and  j==1:
+            for n in range(0,3,2):
+                self.tablero[i-2][j+n-1]=1
+            for n in range(0,3,2):
+                self.tablero[i+n-1][j+2]=1    
+            self.tablero[i][j]=8      
+     
+   ### region 5 para movimientos del caballo
+   
+        if j==0 or j==7:
+            if j==0:
+                u=1
+            if j==7:
+                u=-1
+            if i==1:
+                o=2
+                bandera=1
+            if i==6:
+                o=-2
+                bandera=1
+            if bandera==1:
+                for n in range(0,3,2):
+                    self.tablero[i+n-1][j+(2*u)]=1
+                self.tablero[i+o][j+(1*u)]=1
+                self.tablero[i][j]=8
+    
+        if i==0 or i==7:
+            if i==0:
+                u=1
+            if i==7:
+                u=-1
+            if j==1:
+                o=2
+                bandera=1
+            if j==6:
+                o=-2
+                bandera=1
+            if bandera==1:
+                for n in range(0,3,2):
+                    self.tablero[i+(2*u)][j+n-1]=1
+                self.tablero[i+(1*u)][j+o]=1
+                self.tablero[i][j]=8
+    
+    
+   ### region 6 para movimientos del caballo 
+    
+        if i==j:
+            if j==0:
+                o=1
+                bandera=1
+            if j==7:
+                o=-1
+                bandera=1
+            if bandera==1:
+                self.tablero[i+(2*o)][j+(1*o)]=1
+                self.tablero[i+(1*o)][j+(2*o)]=1    
+                self.tablero[i][j]=8
+                bandera=0
+    
+        if i==0 and  j==7:
+            self.tablero[i+(2)][j-(1)]=1
+            self.tablero[i+(1)][j-(2)]=1    
+            self.tablero[i][j]=88  
+            
+        if i==7 and  j==0:
+            self.tablero[i-(2)][j+(1)]=1
+            self.tablero[i-(1)][j+(2)]=1    
+            self.tablero[i][j]=8
+        
+    
 ## obtener datos
 
 print('El tablero de ajedrez está compuesto por filas y columnas, las filas están enumeradas del 1-8 de manera descendente. Las colunas por otro lado son nombradas con letras de la a-h, a continuación, se muestra un ejemplo: \n')
@@ -115,14 +357,22 @@ while t!=1 and t!=2 and t!=3 and t!=4 and t!=5 and t!=6:
 
 pieza = Pieza(f, ord(c)-96, t)
 
-if pieza.tipo==4:
-    pieza.diagonal()
-    
-if pieza.tipo==3:
-    pieza.ver_hor()
+if pieza.tipo==1:
+    pieza.rey()
 
 if pieza.tipo==2:
     pieza.diagonal()
     pieza.ver_hor()
+
+if pieza.tipo==3:
+    pieza.ver_hor()
+    
+if pieza.tipo==4:
+    pieza.diagonal()
+    
+if pieza.tipo==5:
+    pieza.caballo() 
+
+if pieza.tipo==6:
     
 print (pieza.tablero)
